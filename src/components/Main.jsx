288a3904c513f73aa1c81fa04e934be4a71051c6
jeanmendescs <mendes.jean.cs@gmail.com>
2020-11-25 20:59:08 -0300

    main and presentation styling fixes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,16 +2,20 @@ import React from "react";
 
 const Main = () => {
   return (
-    <section className="main">
-      <div class="main-background-img">
-        <div class="main-background-color"></div>
-      </div>
-      <div>
-        <h1>OTIMIZANDO RESULTADOS NAS ORGANIZAÇÕES</h1>
-        <img
-          src="https://landen.imgix.net/edmyisgcymym/assets/050wj6kt.png?w=1200&h=900&fit=max"
-          alt="Work background"
-        />
+    <section>
+      <div className="main">
+        <div class="main-background-img">
+          <div class="main-background-color"></div>
+        </div>
+        <div>
+          <h1>OTIMIZANDO RESULTADOS NAS ORGANIZAÇÕES</h1>
+          <div className="main-optme">
+            <img
+              src="https://landen.imgix.net/edmyisgcymym/assets/050wj6kt.png?w=1200&h=900&fit=max"
+              alt="Work background"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
