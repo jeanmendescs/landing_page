1d63df2f446995b9cc427a98ec951b64a6e8e9dd
jeanmendescs <mendes.jean.cs@gmail.com>
2020-12-04 19:52:02 -0300

    tech sass file

--- src/components/Tech/index.jsx
+++ src/components/Tech/index.jsx
@@ -1,44 +1,50 @@
 import React from "react";
 import Skill from "../Skill";
+import styles from "./styles.module.scss";
 
 const Tech = () => {
   return (
-    <section className="tech">
-      <div className="container">
+    <section className={styles.tech}>
+      <div className={styles.container}>
         <img
           src="https://landen.imgix.net/edmyisgcymym/assets/gt1kjbf6.png?w=1200&h=900&fit=max"
           alt="Technical Knowledges"
-          className="image"
+          className={styles.image}
         />
         <div>
           <h1>Conhecimentos Técnicos</h1>
           <Skill
-            className="skill"
+            className={styles.skill}
             description="JavaScript"
             icon="check"
             size="20px"
           />
-          <Skill className="skill" description="CSS" icon="check" size="20px" />
           <Skill
-            className="skill"
+            className={styles.skill}
+            description="CSS"
+            icon="check"
+            size="20px"
+          />
+          <Skill
+            className={styles.skill}
             description="HTML"
             icon="check"
             size="20px"
           />
           <Skill
-            className="skill"
+            className={styles.skill}
             description="React"
             icon="check"
             size="20px"
           />
           <Skill
-            className="skill"
+            className={styles.skill}
             description="Redux"
             icon="check"
             size="20px"
           />
           <Skill
-            className="skill"
+            className={styles.skill}
             description="Design Thinking"
             icon="check"
             size="20px"
