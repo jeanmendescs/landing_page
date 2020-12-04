0c4d8ed7a5cdce771fff46bf65c9e551cb51f4ec
jeanmendescs <mendes.jean.cs@gmail.com>
2020-12-04 18:14:36 -0300

    presentation style

--- src/components/Presentation/index.jsx
+++ src/components/Presentation/index.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Skill from "../Skill";
+import styles from "./styles.module.scss";
 
 const Presentation = () => {
   return (
-    <section className="presentation">
-      <div className="container">
+    <section className={styles.presentation}>
+      <div className={styles.container}>
         <div>
           <div>
             <h1>Olá, sou Jean Carlos!</h1>
           </div>
           <div>
-            <div className="whoami">
+            <div className={styles.whoami}>
               <div>
                 <h3>Quem sou eu?</h3>
               </div>
@@ -18,7 +19,7 @@ const Presentation = () => {
                 description="Sou um profissional que está migrando do setor público para a área
               de desenvolvimento front-end"
                 icon="roundedCheck"
-                className="skill-wrapper"
+                className={styles.skill_wrapper}
                 color="rgb(198, 208, 227)"
                 size="104px"
               />
@@ -30,14 +31,14 @@ const Presentation = () => {
               <Skill
                 description="Atuei no setor privado nos ramos de telemarketing, usinagem, e também no setor público."
                 icon="roundedCheck"
-                className="skill-wrapper"
+                className={styles.skill_wrapper}
                 color="rgb(198, 208, 227)"
                 size="104px"
               />
             </div>
           </div>
         </div>
-        <div className="presentation-img">
+        <div>
           <img
             src="https://landen.imgix.net/edmyisgcymym/assets/aqjx17xs.png?w=1200&h=900&fit=max"
             alt="Partner"
