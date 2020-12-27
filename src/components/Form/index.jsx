f4c9c65c5958e3384c9cb421e96a278ecd3a20ae
jeanmendescs <mendes.jean.cs@gmail.com>
2020-12-27 15:06:39 -0300

    refactor input component

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import Input from "../Input";
-import Cleave from "cleave.js/react";
 import { useForm } from "react-hook-form";
 
 const Form = () => {
@@ -17,54 +16,61 @@ const Form = () => {
         <form onSubmit={handleSubmit(onSubmit)}>
           <Input
             name="name"
+            label="Nome"
+            id="name"
             placeholder="Seu nome"
             className={styles.flex}
             ref={register}
           />
           <Input
             className={styles.inlineFirst}
+            id="email"
+            label="Email"
             type="email"
-            name="Email"
+            name="email"
             placeholder="email@exemplo.com"
             ref={register}
           />
           <Input
             className={styles.inlineSecond}
+            label="Empresa"
+            id="company"
             name="company"
             ref={register}
             placeholder="Sua Empresa"
           />
           <Input
+            id="job"
+            label="Cargo"
             name="job"
             placeholder="Seu Cargo"
             ref={register}
             className={styles.inlineFirst}
           />
-          <div className={styles.inlineSecond}>
-            <label>Telefone de contato</label>
-            <Cleave
-              ref={register}
-              name="phone"
-              placeholder="(99) 99999-9999"
-              title="Apenas números"
-              options={{
-                numericOnly: true,
-                blocks: [0, 2, 0, 5, 4],
-                delimiters: ["(", ")", " ", "-"],
-              }}
-            />
-          </div>
-          <div className={styles.flex}>
-            <label htmlFor="proposal">Envie-nos sua proposta ou dúvida</label>
-            <textarea
-              // required
-              id="proposal"
-              name="proposal"
-              rows="5"
-              ref={register}
-              cols="33"
-            ></textarea>
-          </div>
+          <Input
+            className={styles.inlineSecond}
+            label="Telefone de contato"
+            ref={register}
+            id="phone"
+            name="phone"
+            placeholder="(99) 99999-9999"
+            title="Apenas números"
+            maskOptions={{
+              numericOnly: true,
+              blocks: [0, 2, 0, 5, 4],
+              delimiters: ["(", ")", " ", "-"],
+            }}
+          />
+          <Input
+            id="proposal"
+            label="Envie-nos sua proposta ou dúvida"
+            name="proposal"
+            ref={register}
+            cols="33"
+            rows="5"
+            className={styles.flex}
+            textarea
+          />
           <div>
             <button value="submit" type="submit">
               Quero falar com um consultor
--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,12 +1,28 @@
 import React, { forwardRef } from "react";
+import Cleave from "cleave.js/react";
 
-const Input = forwardRef(({ className, name, ...rest }, ref) => {
-  return (
-    <div className={className}>
-      <label htmlFor={name}>{name}</label>
-      <input id={name} name={name} ref={ref} {...rest} />
-    </div>
-  );
-});
+const Input = forwardRef(
+  ({ id, className, maskOptions, label, textarea, ...rest }, ref) => {
+    const renderInput = () => {
+      const commonProps = { id, ref, ...rest };
+      if (textarea) {
+        return <textarea {...commonProps}></textarea>;
+      }
+
+      if (maskOptions) {
+        return <Cleave options={maskOptions} {...commonProps} />;
+      }
+
+      return <input {...commonProps} />;
+    };
+
+    return (
+      <div className={className}>
+        <label htmlFor={id}>{label}</label>
+        {renderInput()}
+      </div>
+    );
+  }
+);
 
 export default Input;
